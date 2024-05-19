21503bace95bc32858ff51e0ee3df1738d948e38
mayados <mariannedos.cours@gmail.com>
2024-05-19 19:28:34 +0200

    create controller with functions index() and mentionslegales()

---
+++ app/src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions_legales')]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('main/mentions_legales.html.twig');
+    }
+
+}
