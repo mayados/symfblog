ca8c3fb7c3d05c17a1235e18bbf2bc39cedf9d19
mayados <mariannedos.cours@gmail.com>
2024-05-30 15:13:16 +0200

    create base of the forms

---
+++ app/src/Form/AddCategoryFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddCategoryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la catégorie'
+            ])
+            ->add('parent', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'placeholder' => '-- Pas de parent --',
+                'required' => false,
+                'query_builder' => function(CategoryRepository $cr){
+                    return $cr->createQueryBuilder('c')
+                        ->orderBy('c.name','ASC');
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
