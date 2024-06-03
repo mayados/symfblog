ed1492c8adbe42b57b9da65340f73db78f2aa0f2
mayados <mariannedos.cours@gmail.com>
2024-06-03 18:45:24 +0200

    add delete on cascade on relation with parent

--- app/src/Entity/Category.php
+++ app/src/Entity/Category.php
@@ -22,6 +22,7 @@ class Category
     private ?string $slug = null;
 
     #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
+    #[ORM\JoinColumn(onDelete: 'CASCADE')]
     private ?self $parent = null;
 
     /**
